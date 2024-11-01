c00a518ce4c3b4dd35fbf2178437076cd21ea5a6
hohlokit <kolik2001@gmail.com>
2024-11-01 07:43:31 +0200

    implemented quote fetching and displaying quote

---
+++ src/components/atoms/index.js
@@ -0,0 +1,4 @@
+export { default as Button } from './Button.vue'
+export { default as Card } from './Card.vue'
+export { default as Triangle } from './Triangle.vue'
+export { default as Loader } from './Loader.vue'
